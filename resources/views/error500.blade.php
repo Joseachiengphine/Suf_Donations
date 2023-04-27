1119057cb81a18a5d760d5a0bafc51ffa9071f8a
Joseachiengphine <joseachiengphine@gmail.com>
2023-04-27 12:51:09 +0300

    Added view

---
+++ resources/views/error500.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Strathmore University Foundation</title>
+    <link rel="stylesheet" href="/css/error500.css" />
+
+</head>
+<body>
+<div class="wrapper">
+    <div class="box">
+        <h1>500</h1>
+        <p>Sorry, it's me, not you.</p>
+        <p>&#58;&#40;</p>
+        <p><a href="/">Let me try again!</a></p>
+    </div>
+</div>
+</body>
