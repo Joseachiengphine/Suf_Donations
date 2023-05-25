e5c9ba553a4607dfa0c36fb7ea588f1068310398
Joseachiengphine <joseachiengphine@gmail.com>
2023-05-25 10:52:22 +0300

    Minor change

--- app/Filament/Pages/MyProfile.php
+++ app/Filament/Pages/MyProfile.php
@@ -1,12 +1,11 @@
 <?php
-/*
+
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
-use Filament\Forms;
 
 
-class MyProfile extends \JeffGreco13\FilamentBreezy\Pages\MyProfile
+class MyProfile extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-user';
 
@@ -15,19 +14,5 @@ class MyProfile extends \JeffGreco13\FilamentBreezy\Pages\MyProfile
 
     protected static string $view = 'filament.pages.my-profile';
 
-    protected function getUpdateProfileFormSchema(): array
-    {
-        $formSchema = parent::getUpdateProfileFormSchema();
-        return [
-            Forms\Components\TextInput::make('name')
-                ->required()
-                ->label(__('filament-breezy::default.fields.name')),
-            Forms\Components\TextInput::make($this->loginColumn)
-                ->required()
-                ->email(fn () => $this->loginColumn === 'email')
-                ->unique(config('filament-breezy.user_model'), ignorable: $this->user)
-                ->label(__('filament-breezy::default.fields.email')),
-        ];
-    }
 
-}*/
+}
